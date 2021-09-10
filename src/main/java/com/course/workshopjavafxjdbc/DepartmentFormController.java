0de680ed8b94e5c2d3338aa3b82d06e0011cd512
lucaschiavicatti <56878253+lucaschiavicatti@users.noreply.github.com>
2021-09-10 12:30:48 -0300

    DepartmentFormController

---
+++ src/main/java/com/course/workshopjavafxjdbc/DepartmentFormController.java
@@ -0,0 +1,49 @@
+package com.course.workshopjavafxjdbc;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import util.Constraints;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSaveAction() {
+        System.out.println("teste");
+    }
+
+    @FXML
+    public void onBtCancelAction() {
+        System.out.println("teste1");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+}
